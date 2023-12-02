34a3de33799e2ca0c534b4892f759898db62ba1d
NataliaPolak13 <272227@student.pwr.edu.pl>
2023-12-02 01:16:13 +0100

    changes in FirstHomework

--- FirstHomework/Program.cs
+++ FirstHomework/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace FirstHomework
+﻿using System;
+
+namespace FirstHomework
 {
     internal class Program
     {
@@ -6,20 +8,57 @@
         {
             while (true)
             {
-                Console.Write("Enter a mathematical expression or type exit to quit: ");
-                string expression = Console.ReadLine();
-
-                if (expression.ToLower() == "exit")
-                    break;
-
                 try
                 {
-                    //Console.WriteLine("Your result is: " + Calculator(expression));
+                    Console.Write("Enter a mathematical expression (like 1 + 2) or type exit to quit: ");
+                    string expression = Console.ReadLine();
+                    if (expression.ToLower() == "exit")
+                        break;
+
+                    Calculator(expression);
+                    Clear();
                 }
                 catch (Exception e)
                 {
                     Console.WriteLine(e.Message);
                 }
+
+
+
+                static double GetNumber()
+                {
+                    if (!double.TryParse(Console.ReadLine(), out double input))
+                        throw new Exception("Your input is not a number. Try again. ");
+
+                    return input;
+                }
+
+                static void Clear()
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("If you want to exit write 1");
+                    Console.WriteLine("If you want to continue write 2");
+                    Console.Write("Please choose what you would like to do: ");
+                    var Option = GetNumber();
+
+                    switch (Option)
+                    {
+                        case 1:
+                            Console.WriteLine("Bye!");
+                            Environment.Exit(0);
+                            break;
+
+                        case 2:
+                            Console.Clear();
+                            break;
+                        default:
+                            Console.WriteLine("Incorrect output. Try again.");
+                            break;
+
+                    }
+                }
+
+
             }
 
 
@@ -49,10 +88,6 @@
 
         static void Exponentiation(double x, double y)
         {
-            if (y == 0)
-            {
-                Console.WriteLine("Your result is: 1");
-            }
 
             double result = Math.Pow(x, y);
 
@@ -60,49 +95,30 @@
 
         }
 
-        static void Factorial(ulong x, ulong y)
+        static void Factorial(ulong x)
         {
-            if ((x < 0) || (y < 0))
+            if (x < 0)
             {
                 Console.WriteLine("Factorial is not possible for negative numbers ");
 
             }
-            else if ((x > 20) || (y > 20))
-            {
-                if ((x > 20) && (y > 20))
-                {
-                    Console.WriteLine();
-                    Console.WriteLine("Both numbers are to high. I can only count to number 20. Please try again.");
-                }
-                else if (y > 20)
-                {
-                    Console.WriteLine();
-                    Console.WriteLine("Second number is to high. I can only count to number 20. Please try again.");
-                }
-                else
-                    Console.WriteLine("First number is to high. I can only count to number 20. Please try again.");
-
-            }
-            else if ((x == 0) || (y == 0))
+            
+            else if (x == 0) 
             {
                 Console.WriteLine("Your result is: 1");
             }
+
             else
             {
                 ulong resultx = 1;
-                ulong resulty = 1;
 
-                for (ulong i = y; i > 1; i--)
-                {
-                    resulty *= i;
-                }
 
                 for (ulong i = x; i > 1; i--)
                 {
                     resultx *= i;
                 }
 
-                Console.WriteLine($"Your result is: {resultx} for x, and {resulty} for y");
+                Console.WriteLine($"Your result is: {resultx} for x");
             }
 
         }
@@ -112,9 +128,72 @@
             expression = expression.Replace(" ", "");
 
             if (string.IsNullOrWhiteSpace(expression))
-                Console.WriteLine("Your input is empty.");
+                throw new Exception("Your input is empty.");
 
-            string[] parts = expression.Split('+', '-', '*', '/', '^');
+
+            string operation = "";
+            double firstNumber = 0;
+            double secondNumber = 0;
+            char[] signs = ['!', '-', '+', '/', '*', '^'];
+
+            for(int i = 0; i < signs.Length; i++)
+            {
+                /*
+                 * if before first number is one of signes we need to secure 
+                 * operation of splitting string
+                 * but tommorrow :)
+                 */
+                string[] parts = expression.Split(signs[i]);
+                if (parts[0] != expression)
+                {
+                    operation = signs[i].ToString();
+                    firstNumber = Convert.ToDouble(parts[0]);
+
+                    if (signs[i] != '!') {
+                        if (parts.GetLength(0) > 2)
+                            parts[1] = operation + parts[2];
+                        var d = parts.GetLength(0);
+                            secondNumber = Convert.ToDouble(parts[1]);
+                    }
+
+                    break;
+                }
+
+            }
+
+
+            switch (operation)
+            {
+
+                case "+":
+                    Addition(firstNumber, secondNumber);
+                    break;
+
+                case "-":
+                    Substraction(firstNumber, secondNumber);
+                    break;
+
+                case "*":
+                    Multipication(firstNumber, secondNumber);
+                    break;
+
+                case "/":
+                    Division(firstNumber, secondNumber);
+                    break;
+
+                case "^":
+                    Exponentiation(firstNumber, secondNumber);
+                    break;
+
+                case "!":
+                    Factorial((ulong)firstNumber);
+                    break;
+
+                default:
+                    Console.WriteLine("Incorrect output. Try again.");
+                    break;
+
+            }
 
         }
     }
